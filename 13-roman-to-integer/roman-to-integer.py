551a6170ec2e08f480397913ae77c11960c2ba81
Seshagiri_Mallela <mallelasashi@gmail.com>
2026-03-25 18:38:57 +0530

    Time: 5 ms (52.14%) | Memory: 19.2 MB (78.24%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -9,13 +9,13 @@ class Solution:
             'D': 500,
             'M': 1000
         }
-        
+
         total = 0
-        
+
         for i in range(len(s)):
             if i > 0 and roman[s[i]] > roman[s[i-1]]:
                 total += roman[s[i]] - 2 * roman[s[i-1]]
             else:
                 total += roman[s[i]]
-                
+
         return total
